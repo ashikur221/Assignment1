4f99c5c973529c3a706578892ffdae709104b050
ashikur-ai <projectshortlife@gmail.com>
2025-05-21 22:00:48 +0600

    first problem solved

--- task.ts
+++ task.ts
@@ -1,1 +1,11 @@
-console.log('hello')
+const formatString = (givenString: string, option: boolean = true): string => {
+  if (option) {
+    return givenString.toUpperCase();
+  } else {
+    return givenString.toLowerCase();
+  }
+}
+
+console.log(formatString("Hello"))
+console.log(formatString("Hello", true))
+console.log(formatString("Hello", false))
