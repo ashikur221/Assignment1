8d6212288089305bf2b16a86d2d9c0d2a4036cf1
ashikur-ai <projectshortlife@gmail.com>
2025-05-24 22:59:13 +0600

    finish the assignment

---
+++ README.md
@@ -0,0 +1,14 @@
+#The difference between interface and types are given below:
+
+the interface can extend other interface or types but in type need to intersection. In this way both can mutate to a different kind of each type.
+
+If you can about merging then interface supports it but not the types. And the main difference is the interface is mostly used for object to gain readability but type uses on others.
+
+
+# TypeScript helps in improving code quality and project maintainability.
+
+If we talk about the type safety it is the main characteristics. This prevent bugs at compile time. It has better autocomplete and intellisence. It is self documented. So, like interface and types helps coder understand what it says. 
+
+As it strict to follow one nich for which easy to scale up. Cause every developer can understand how the things work round.
+
+For all this big company like google, microsoft etc. use typescript to reduce bugs in production, speed up development.
--- task.ts
+++ task.ts
@@ -67,5 +67,84 @@ class Car extends Vehicle{
 }
 
 const myCar = new Car("Toyota", 2020, "Corolla");
-console.log(myCar.getInfo())
-console.log(myCar.getModel())
+// console.log(myCar.getInfo())
+// console.log(myCar.getModel())
+
+function processValue(value: string | number): number{
+  if (typeof value === 'string') {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+
+
+// console.log(processValue("hello"))
+// console.log(processValue(10));
+
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null{
+  if (products.length === 0) {
+    return null;
+  }
+
+  let result = products[0];
+
+  for (const item of products) {
+    if (item.price > result.price) {
+      result = item;
+    }
+  }
+
+  return result;
+}
+
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 },
+];
+
+// console.log(getMostExpensiveProduct(products));
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+
+function getDayType(day: Day): string{
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  }
+  return "Weekday"
+}
+
+// console.log(getDayType(Day.Monday))
+// console.log(getDayType(Day.Sunday))
+
+async function squareAsync(n:number): Promise <number> {
+  if (n < 0) {
+    throw new Error('Negative number not allowed')
+  }
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(n * n);
+    }, 1000)
+  });
+}
+
+
+// squareAsync(4).then(console.log)
+// squareAsync(-3).catch(console.error)
